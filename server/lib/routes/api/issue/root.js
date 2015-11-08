bfbd66b55fd1c4a2f96d41e16789341fb9b4d7c0
Adam Lee <adam@ninjastack.com>
2015-11-07 19:25:14 -0500

    reorganize

---
+++ server/lib/routes/api/issue/root.js
@@ -0,0 +1,10 @@
+var geocoder = require('geocoder');
+
+var mongoose = require('mongoose');
+var router = require('express').Router({ mergeParams: true });
+
+router.get('/', function(req, res) {
+  
+});
+
+module.exports = router;
