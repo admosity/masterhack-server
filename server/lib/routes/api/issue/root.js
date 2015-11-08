d57f9a0bf815e71aeff7770db3daf9bd26d3c30a
Adam Lee <adam@ninjastack.com>
2015-11-07 19:38:20 -0500

    create issue

--- server/lib/routes/api/issue/root.js
+++ server/lib/routes/api/issue/root.js
@@ -1,10 +1,47 @@
-var geocoder = require('geocoder');
-
 var mongoose = require('mongoose');
-var router = require('express').Router({ mergeParams: true });
+var router = require('express').Router({
+  mergeParams: true
+});
+var isLoggedIn = require('lib/middleware/isLoggedIn');
 
-router.get('/', function(req, res) {
-  
+
+var Issue = mongoose.model('Issue');
+
+router.route('/')
+
+.post(isLoggedIn, function(req, res) {
+
+  var image = req.body.image;
+  var description = req.body.description;
+  var pledged = req.body.pledged;
+  var loc = req.body.loc;
+
+  if (!image || !description || !pledged || !loc) {
+    return res.error(400, '7f78cfaa-8db5-42e5-9ffd-c3446cba2675');
+  }
+
+  var issue = new Issue({
+    image: image
+    description: description
+    pledged: pledged
+    loc: loc,
+    pledgers: [{
+      who: req.user,
+      amount: pledged
+    }]
+  });
+
+  // TODO: add payment
+
+  issue.save(function(err) {
+    if (err) return res.error(500, 'af55fd5d-9e04-417f-b826-0385aaacba5b');
+    return res.ok(issue);
+  });
+
+})
+
+.get(function(req, res) {
+
 });
 
 module.exports = router;
