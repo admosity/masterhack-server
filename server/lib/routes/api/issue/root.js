914fdd1c2c38347c339ec783257d24cae609b663
David Kim <davidk@halopowered.com>
2015-11-07 19:45:11 -0500

    Added sandbox keys for simplify

--- server/lib/routes/api/issue/root.js
+++ server/lib/routes/api/issue/root.js
@@ -1,8 +1,35 @@
 var mongoose = require('mongoose');
+var nconf = require('nconf');
 var router = require('express').Router({
   mergeParams: true
 });
 var isLoggedIn = require('lib/middleware/isLoggedIn');
+
+client = Simplify.getClient({
+  publicKey: nconf.get('SIMPLIFY_COMMERCE_PUBLIC'),
+  privateKey: nconf.get('SIMPLIFY_COMMERCE_PRIVATE')
+});
+
+function processPayment(token, amount, referenceId, cb){
+  //Multiply amount by 100 since 1000 = $10
+  client.payment.create({
+      amount : amount * 100,
+      token : token,
+      description : "payment description",
+      reference : referenceId,
+      currency : "USD"
+  }, function(errData, data){
+      if(errData){
+          console.error("Error Message: " + errData.data.error.message);
+          // handle the error
+          cb(errData.data.error.message);
+          return;
+      }
+   
+      console.log("Payment Status: " + data.paymentStatus);
+      cb(null, data.paymentStatus);
+  });
+}
 
 
 var Issue = mongoose.model('Issue');
